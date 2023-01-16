e520127c5b62d36a2b65e8933fe63ba1a051acda
Olha <olga.yarukhina@gmail.com>
2023-01-16 17:38:21 +0200

    pathes no crossed

--- functions/getPathes.go
+++ functions/getPathes.go
@@ -14,9 +14,10 @@ func AllPaths(s ,f *Cell, c []*Cell) ([][]*Cell){
 
 	Label:
 	for i:=0; i<len(s.Tunnels); i++ {
+		eachPP := eachP
 		if s.Tunnels[i] == f {
-		eachP = append(eachP, s.Tunnels[i])
-		allp = append(allp, eachP)
+		eachPP = append(eachPP, s.Tunnels[i])
+		allp = append(allp, eachPP)
 		continue
 		} else {
 		for j:= 0; j<len(eachP); j++ {   //check if we have already been in that cell
@@ -41,27 +42,45 @@ func AllSolutions(p [][]*Cell) ([][][]*Cell) {
 	sort.Slice(srtd, func(i, j int) bool {
 		return len(srtd[i]) < len(srtd[j])
 	})
+
+	
 	ar = append(ar, srtd[0])
 	sol = append(sol, ar)
+	fmt.Println(ar)
+	fmt.Print("here")
+	fmt.Println(srtd[0][1])
+
 
 	Lable:
 	for i:=1; i<len(srtd); i++ {
-		for j:=1; j<len(srtd[i]); j++{ //without start and finish
-			check := false
+		//fmt.Println(i)
+		//fmt.Println(srtd[i])
+		check := false
+		for j:=1; j<len(srtd[i]); j++{ 
+			//fmt.Println(srtd[i][j])
 			for k:=0; k<len(ar); k++ {		
 				for _, cell := range ar[k]{
-				if cell == srtd[i][j] && cell != srtd[i][0] && cell != srtd[i][len(srtd[i])-1] {
+				if cell == srtd[i][j] && cell != srtd[0][0] && cell != srtd[0][len(srtd[0])-1] { //without start and finish
+					fmt.Println("here1")
+					fmt.Println(srtd[i][j])
+					fmt.Println(srtd[0][0])
+					fmt.Println(srtd[0][len(srtd[0])-1] )
 					check = true
 					continue Lable
-				}		
+					}		
 				}
 				}	
 				if check == false {
 					ar = append(ar, srtd[i])
+					fmt.Println("arr")
+						fmt.Println(ar)
 			}
+		
+
 		}
 	}	
-	fmt.Println(ar)
+	fmt.Println("Good:")
+fmt.Println(ar) //ways without bottle
 	return sol
 }
 
--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"fmt"
-	"sort"
 
 	"01.kood.tech/git/oyarukhi/lem-in/functions"
 )
 
 func main(){
-
-	
 
 	//1. create Cells with tunnels
 	
@@ -17,32 +14,29 @@ func main(){
 	n, start, end, cells:= functions.ParsData(getData)
 
 	fmt.Println(n)
-	fmt.Println(cells[0].Tunnels[0])	
-
-	fmt.Println(cells)
-
+	fmt.Println(cells)	
 
 	// 2. create Path
 
     var c []*functions.Cell
-
 	path := functions.AllPaths(start, end, c)
-	fmt.Println("Arr of pathes:")
-	fmt.Println(path)
-	for _, r := range path {
+
+
+
+	// 3. strategy
+	
+allSollutions := functions.AllSolutions(path)
+fmt.Println("Need:")
+		fmt.Println(allSollutions)
+}
+
+
+/*
+for _, r := range path {
 		fmt.Println("next")
 		for _, g := range r{
 			fmt.Println(g)
 		}
 		
 	}
-
-	// 3. strategy
-	
-	allSollutions := functions.AllSolutions(path)
-	
-	sort.Slice(path, func(i, j int) bool {
-		return len(path[i]) < len(path[j])
-	})
-		fmt.Println(allSollutions)
-}
+	*/
